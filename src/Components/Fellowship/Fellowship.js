8e32aa223e7ff6d9565e536d811ee493315b16ad
Delilah Necrason <delilahnecrason@gmail.com>
2022-01-18 18:57:47 -0500

    Add router functionality to character image on fellowship page

--- src/Components/Fellowship/Fellowship.js
+++ src/Components/Fellowship/Fellowship.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Character from '../Character/Character';
 import PropTypes from 'prop-types';
 import './Fellowship.css';
 
@@ -6,24 +8,32 @@ const Fellowship = ({team, findPlayer, removePlayer}) => {
 
 
   let teamMembers;
+  let fellowshipHeader;
 
   team.length ? 
 
   teamMembers = team.map(player => {
     
     const firstName = player.name.split(' ')[0];
+    const image = player.image;
+    const key = player._id;
    
     return (
       <div className="player-card">
-        <img src={player.image} alt={player.name} className='grid-img' id={player._id} onClick={() => findPlayer(player.name)}></img>
+        <Link to={`/${player.name.split(' ')[0]}`} style={{textDecoration: 'none'}}>
+          <Character key={key} player={player} stats={player[1]} image={image} findPlayer={findPlayer}></Character>
+        </Link>
         <button className="remove-btn" onClick={() => removePlayer(player)}>Remove {firstName}</button>
       </div>
     )
   })
   : teamMembers = <p className="no-team-msg">You haven't added anyone to your Fellowship yet.</p>
 
+  team.length ? fellowshipHeader = <header className="fellowship-header">You have chosen.</header> : fellowshipHeader = <header className="fellowship-header">Let's not be hasty!</header>
+
   return (
     <>
+      {fellowshipHeader}
       <section className="points-header">
         {/* <h2>Your Fellowship's Current Scores:</h2> */}
       </section>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
         <h1 className="header-title"><Link to="/" class="link">Fantasy Fellowship</Link></h1>
       </div>
       <div className="nav-container">
+        <p className="header-link"><Link to="/" class="link">Players</Link></p>
         <p className="header-link"><Link to="/fellowship" class="link">Your Fellowship</Link></p>
       </div>
     </section>
